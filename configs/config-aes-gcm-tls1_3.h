58e2f5c5d61f414cb7c822789683acd5bbc6fced
Nadav Tasher <tashernadav@gmail.com>
2024-11-23 17:47:33 +0200

    Final configurations

--- configs/config-aes-gcm-tls1_3.h
+++ configs/config-aes-gcm-tls1_3.h
@@ -1,7 +1,7 @@
 /**
  * \file config-aes-gcm-tls1_3.h
  *
- * \brief Minimal configuration for TLS 1.3 with AES-GCM ciphersuites
+ * \brief Minimal configuration for a TLS 1.3 only client with AES-GCM ciphersuites
  */
 /*
  *  Copyright The Mbed TLS Contributors
@@ -18,20 +18,42 @@
 /* Other MBEDTLS_HAVE_XXX flags irrelevant for this configuration */
 
 /* Mbed TLS modules */
+#define MBEDTLS_ASN1_PARSE_C
+#define MBEDTLS_ASN1_WRITE_C
+#define MBEDTLS_CTR_DRBG_C
+#define MBEDTLS_ENTROPY_C
+#define MBEDTLS_MD_C
 #define MBEDTLS_NET_C
-#define MBEDTLS_GCM_C
-#define MBEDTLS_AES_C
+#define MBEDTLS_OID_C
+#define MBEDTLS_PK_C
+#define MBEDTLS_PK_PARSE_C
 #define MBEDTLS_SSL_CLI_C
 #define MBEDTLS_SSL_TLS_C
-#define MBEDTLS_ENTROPY_C
-#define MBEDTLS_CTR_DRBG_C
 #define MBEDTLS_X509_CRT_PARSE_C
+#define MBEDTLS_X509_USE_C
+
+/* Enable ECP algorithms */
+#define MBEDTLS_ECP_DP_BP256R1_ENABLED
+#define MBEDTLS_ECP_DP_BP384R1_ENABLED
+#define MBEDTLS_ECP_DP_BP512R1_ENABLED
+#define MBEDTLS_ECP_DP_CURVE25519_ENABLED
+#define MBEDTLS_ECP_DP_CURVE448_ENABLED
+#define MBEDTLS_ECP_DP_SECP192K1_ENABLED
+#define MBEDTLS_ECP_DP_SECP192R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP224K1_ENABLED
+#define MBEDTLS_ECP_DP_SECP224R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP256K1_ENABLED
+#define MBEDTLS_ECP_DP_SECP256R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP384R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP521R1_ENABLED
 
 /* TLS protocol feature support */
 #define MBEDTLS_SSL_PROTO_TLS1_3
 #define MBEDTLS_SSL_KEEP_PEER_CERTIFICATE
-#define MBEDTLS_KEY_EXCHANGE_WITH_CERT_ENABLED
+#define MBEDTLS_SSL_TLS1_3_COMPATIBILITY_MODE
+#define MBEDTLS_KEY_EXCHANGE_ECDH_ECDSA_ENABLED
 #define MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_EPHEMERAL_ENABLED
+
 /*
  * Use only AES-GCM ciphersuites, and
  * save ROM and a few bytes of RAM by specifying our own ciphersuite list
@@ -47,6 +69,6 @@
 
 /* Error messages and TLS debugging traces
  * (huge code size increase, needed for tests/ssl-opt.sh) */
-#define MBEDTLS_DEBUG_C
-#define MBEDTLS_ERROR_C
+//#define MBEDTLS_DEBUG_C
+//#define MBEDTLS_ERROR_C
 
--- configs/crypto-config-aes-gcm-tls1_3.h
+++ configs/crypto-config-aes-gcm-tls1_3.h
@@ -1,8 +1,7 @@
 /**
- * \file crypto-config-ccm-psk-tls1_2.h
+ * \file crypto-config-aes-gcm-tls1_3.h
  *
- * \brief Minimal crypto configuration for TLS 1.2 with
- * PSK and AES-CCM ciphersuites
+ * \brief Minimal crypto configuration for a TLS 1.3 only client with AES-GCM ciphersuites
  */
 /*
  *  Copyright The Mbed TLS Contributors
@@ -10,19 +9,21 @@
  */
 
 /**
- * To be used in conjunction with configs/config-ccm-psk-tls1_2.h
- * or configs/config-ccm-psk-dtls1_2.h. */
+ * To be used in conjunction with configs/config-aes-gcm-tls1_3.h */
 
 #ifndef PSA_CRYPTO_CONFIG_H
 #define PSA_CRYPTO_CONFIG_H
 
 #define PSA_WANT_ALG_GCM                        1
 #define PSA_WANT_ALG_ECDH                       1
+#define PSA_WANT_ALG_ECDSA                      1
 #define PSA_WANT_ALG_SHA_256                    1
 #define PSA_WANT_ALG_SHA_384                    1
 #define PSA_WANT_ALG_HKDF_EXPAND                1
 #define PSA_WANT_ALG_HKDF_EXTRACT               1
 
-#define PSA_WANT_KEY_TYPE_AES               1
+#define PSA_WANT_KEY_TYPE_AES                   1
+#define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_BASIC    1
+#define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_GENERATE 1
 
 #endif /* PSA_CRYPTO_CONFIG_H */
